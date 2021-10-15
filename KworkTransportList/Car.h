a5d05ca387d97703c0f86ba820cddcd20b42165e
Maxim <max77648@gmail.com>
2021-10-15 15:55:33 +0300

    Вынес Car в отдельный файл

---
+++ KworkTransportList/Car.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include "Transport.h"
+
+class Car : Transport
+{
+private:
+	double _mileage;
+
+public:
+	Car() : _mileage(), Transport() {}
+	Car(double mileage, std::string name, unsigned int countOfSeats, std::string dateOfBirth, std::string color, double weight)
+		: Transport(name, countOfSeats, dateOfBirth, color, weight)
+	{
+		_mileage = mileage;
+	}
+
+	void ShowInfo()
+	{
+		std::cout << "Car\n";
+		PrintInfo();
+	}
+
+	void Move()
+	{
+		std::cout << "Я - машина. Еду по дороге.";
+	}
+};
--- KworkTransportList/main.cpp
+++ KworkTransportList/main.cpp
@@ -34,30 +34,6 @@
 
 using namespace std;
 
-class Car : Transport
-{
-private:
-	double _mileage;
-
-public:
-	Car() : _mileage(), Transport() {}
-	Car(double mileage, std::string name, unsigned int countOfSeats, std::string dateOfBirth, std::string color, double weight)
-		: Transport(name, countOfSeats, dateOfBirth, color, weight)
-	{
-		_mileage = mileage;
-	}
-
-	void ShowInfo()
-	{
-		std::cout << "Car\n";
-		PrintInfo();
-	}
-
-	void Move()
-	{
-		std::cout << "Я - машина. Еду по дороге.";
-	}
-};
 
 
 
