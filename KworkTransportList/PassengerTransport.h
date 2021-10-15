0c0d3466ed61cdbcf1d29a5ec28ac864c9a6fe00
Maxim <max77648@gmail.com>
2021-10-15 17:20:24 +0300

    Начал делать меню ввода

---
+++ KworkTransportList/PassengerTransport.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "TypeTransport.h"
+
+class PassengerTransport : TypeTransport
+{
+private:
+	unsigned int _countPassengers;
+public:
+	PassengerTransport() : _countPassengers(0), TypeTransport() {}
+	PassengerTransport(int countPassengers, Transport tansport, std::string pointA, std::string pointB) : TypeTransport(tansport, pointA, pointB)
+	{
+		_countPassengers = countPassengers;
+	}
+
+	void ShowInfo()
+	{
+		std::cout << "Пассажирский тип\n";
+		std::cout << "Кол-во пассажиров: " << _countPassengers << " ч.\n";
+		PrintInfo();
+	}
+};
--- KworkTransportList/main.cpp
+++ KworkTransportList/main.cpp
@@ -1,11 +1,13 @@
-#include <string>
 #include <iostream>
+#include <string>
+
 #include "Transport.h"
 #include "Car.h"
 #include "Train.h"
 #include "Airplane.h"
 #include "TypeTransport.h"
 #include "Cargo.h"
+#include "PassengerTransport.h"
 
 /*
 Сам вариант:  Создать базовый класс «Транспорт» и производные от него классы «Автомобиль», «Поезд», «Самолет».
@@ -40,31 +42,166 @@
 */
 
 using namespace std;
-class PassengerTransport : TypeTransport
+
+Transport inputTransport()
 {
-private:
-	unsigned int _countPassengers;
-public:
-	PassengerTransport() : _countPassengers(0), TypeTransport() {}
-	PassengerTransport(int countPassengers, Transport tansport, std::string pointA, std::string pointB) : TypeTransport(tansport, pointA, pointB)
+	std::string name;
+	unsigned int countofSeats;
+	std::string dateOfBirth;
+	std::string color;
+	double weight;
+	std::cout << "Введите информацию о транспорте:\n";
+	std::cout << "Введите название: ";
+	cin >> name;
+	std::cout << "Введите кол-во мест для посадки: ";
+	cin >> countofSeats;
+	std::cout << "Введите дату выпуска вида(дд:мм:гггг): ";
+	cin >> dateOfBirth;
+	std::cout << "Введите цвет: ";
+	cin >> color;
+	std::cout << "Введите вес: ";
+	cin >> weight;
+
+	return Transport(name, countofSeats, dateOfBirth, color, weight);
+}
+
+TypeTransport inputTypeTransport()
+{
+	std::string pointA;
+	std::string pointB;
+	std::cout << "Введите информацию о типе транспорта: ";
+	std::cout << "Введите пункт отправки: ";
+	cin >> pointA;
+	std::cout << "Введите пункт назначения: ";
+	cin >> pointB;
+	Transport transport = inputTransport();
+
+	return TypeTransport(transport, pointA, pointB);
+}
+
+Transport inputDifferentTransport()
+{
+	int numberCommand;
+	std::string userInput;
+	Transport transport;
+
+	std::string name;
+	unsigned int countofSeats;
+	std::string dateOfBirth;
+	std::string color;
+	double weight;
+
+	std::cout << "Выберете транспорт:\n";
+	std::cout << "1) Машина\n";
+	std::cout << "2) Поезд\n";
+	std::cout << "3) Самолёт\n";
+	cin >> userInput;
+	try
 	{
-		_countPassengers = countPassengers;
+		std::cout << "Введите информацию о транспорте:\n";
+		std::cout << "Введите название: ";
+		cin >> name;
+		std::cout << "Введите кол-во мест для посадки: ";
+		cin >> countofSeats;
+		std::cout << "Введите дату выпуска вида(дд:мм:гггг): ";
+		cin >> dateOfBirth;
+		std::cout << "Введите цвет: ";
+		cin >> color;
+		std::cout << "Введите вес: ";
+		cin >> weight;
+	}
+	catch (const std::exception&)
+	{
+		numberCommand = 4;
 	}
 
-	void ShowInfo()
+	switch (numberCommand)
 	{
-		std::cout << "Пассажирский тип\n";
-		std::cout << "Кол-во пассажиров: " << _countPassengers << " т.\n";
-		PrintInfo();
+	case 1:
+	{
+		double mileage;
+		std::cout << "Вы выбрали Машину\n";
+		std::cout << "Введите км, которое она прошла:";
+		cin >> mileage;
+		transport = Car(mileage, name, countofSeats, dateOfBirth, color, weight);
+		break;
 	}
-};
+	case 2:
+	{
+		double raiGage;
+		std::cout << "Вы выбрали Поезд\n";
+		std::cout << "Введите ширину рельс по которым он ездит:";
+		cin >> raiGage;
+		transport = Train(raiGage, name, countofSeats, dateOfBirth, color, weight);
+		break;
+	}
+	case 3:
+	{
+		double flightAltitude;
+		std::cout << "Вы выбрали Самолёт\n";
+		std::cout << "Введите высоту, на которой он летает:";
+		cin >> flightAltitude;
+		transport = Airplane(flightAltitude, name, countofSeats, dateOfBirth, color, weight);
+		break;
+	}
+	default:
+		std::cout << "Неизвестная команда!\n";
+		break;
+	}
+	return transport;
+}
 
+void ShowMenu()
+{
+	bool isExist = false;
+	int numberCommand;
+	std::string userInput;
+	while (isExist == false)
+	{
+		std::cout << "Выберете тип транспорта:\n";
+		std::cout << "1) Грузовой\n";
+		std::cout << "2) Пассажирский\n";
+		std::cout << "3) Выход из программы\n";
+		cin >> userInput;
+		try
+		{
+			numberCommand = stoi(userInput);
+		}
+		catch (const std::exception&)
+		{
+			numberCommand = 4;
+		}
+
+		switch (numberCommand)
+		{
+		case 1:
+		{
+			std::cout << "Вы выбрали Грузовой\n";
+			break;
+		}
+		case 2:
+		{
+			std::cout << "Вы выбрали Пассажирский\n";
+			break;
+		}
+		case 3:
+		{
+			isExist = true;
+			break;
+		}
+		default:
+			std::cout << "Неизвестная команда!\n";
+			break;
+		}
+	}
+	std::cout << "Программа закончила свою работу.\n";
+}
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	Transport transport;
-
+	ShowMenu();
+	Transport transport = inputTransport();
 	transport.ShowInfo();
 	transport.Move();
 	std::cout << '\n';
@@ -92,7 +229,7 @@ int main()
 	cargo.ShowInfo();
 	std::cout << '\n';
 
-	PassengerTransport passengerTransport;
+	PassengerTransport passengerTransport(10, train, "Россия", "Китай");
 	passengerTransport.ShowInfo();
 	system("pause");
 	return 0;
