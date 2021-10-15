0f2b82dc0f5f94834ba8460fa453b6ef3dac0003
Maxim <max77648@gmail.com>
2021-10-15 15:54:31 +0300

    Add project files.

---
+++ KworkTransportList/main.cpp
@@ -0,0 +1,77 @@
+#include <string>
+#include <iostream>
+#include "Transport.h"
+
+/*
+Сам вариант:  Создать базовый класс «Транспорт» и производные от него классы «Автомобиль», «Поезд», «Самолет».
+Создать базовый класс «Тип» и производные от него классы «Грузовой» и «Пассажирский».
+На основе этих классов создать набор различных классов с использованием множественного наследования.
+
+Что общего между машиной, поездом, самолётом ?
+Поля класса Транспорт
+1) у них есть название
+2) посадочных мест
+3) дата выпуска
+4) цвет
+5) вес
+6) .... и хватит
+Методы
+1) получение каждого поля
+2) метод перемещение
+3) способ заправки
+
+Что у них разного ?
+Машина
+	1) едет по дороге
+	2) заправляется где угодно, где есть бензин
+Поезд
+	1) едет по рельсам
+	2) заправляется где угодно, где есть бензин
+Самолёт
+	1) летит
+	2) заправляется в аэропорту
+*/
+
+using namespace std;
+
+class Car : Transport
+{
+private:
+	double _mileage;
+
+public:
+	Car() : _mileage(), Transport() {}
+	Car(double mileage, std::string name, unsigned int countOfSeats, std::string dateOfBirth, std::string color, double weight)
+		: Transport(name, countOfSeats, dateOfBirth, color, weight)
+	{
+		_mileage = mileage;
+	}
+
+	void ShowInfo()
+	{
+		std::cout << "Car\n";
+		PrintInfo();
+	}
+
+	void Move()
+	{
+		std::cout << "Я - машина. Еду по дороге.";
+	}
+};
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	Transport transport;
+
+	transport.ShowInfo();
+	transport.Move();
+
+	Car car(123.3, "Ford", 5, "12:12:2020", "Red", 1.2);
+	car.ShowInfo();
+	car.Move();
+	system("pause");
+	return 0;
+}
